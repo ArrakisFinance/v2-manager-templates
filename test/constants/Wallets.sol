6786ad64f4f3b4fde77111c20addc237d1aae7a4
Gevarist <98388264+Gevarist@users.noreply.github.com>
2023-01-10 15:18:16 +0100

    Feat/foundry integration
    * chore: refacto
    
    * fix: dynamic element add on array
    
    * test: simpleManagerTWAP
    
    * fix: checkMinReturn formula
    
    * chore: clean up
    
    * chore: slippage check
    
    Co-authored-by: kassandra <0xdada@protonmail.com>
    
    Co-authored-by: kassandra <0xdada@protonmail.com>
    
    Co-authored-by: kassandra <0xdada@protonmail.com>

---
+++ test/constants/Wallets.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+/// @dev polygon wallet
+
+address constant binanceUSDCHotWallet = 0xF977814e90dA44bFA03b6295A0616a897441aceC;
+address constant aaveWETHPool = 0x28424507fefb6f7f8E9D3860F56504E4e5f5f390;
