787d619f320fbb2e96299ae801520efe75efc1f2
gevarist <gevariste@protonmail.com>
2023-05-23 16:46:46 +0200

    deploy: goerli

---
+++ deploy/SimpleManager.deploy.ts
@@ -0,0 +1,45 @@
+import { deployments, getNamedAccounts } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { getAddresses } from "../src/addresses";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  if (
+    hre.network.name === "mainnet" ||
+    hre.network.name === "polygon" ||
+    hre.network.name === "goerli" ||
+    hre.network.name === "optimism" ||
+    hre.network.name === "arbitrum" ||
+    hre.network.name === "binance"
+  ) {
+    console.log(
+      `Deploying SimpleManager to ${hre.network.name}. Hit ctrl + c to abort`
+    );
+    await new Promise((r) => setTimeout(r, 20000));
+  }
+
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const addresses = getAddresses(hre.network.name);
+
+  await deploy("SimpleManager", {
+    from: deployer,
+    args: [addresses.UniswapV3Factory],
+    log: true,
+  });
+};
+
+export default func;
+
+func.skip = async (hre: HardhatRuntimeEnvironment) => {
+  const shouldSkip =
+    hre.network.name === "mainnet" ||
+    hre.network.name === "polygon" ||
+    hre.network.name === "goerli" ||
+    hre.network.name === "optimism" ||
+    hre.network.name === "arbitrum" ||
+    hre.network.name === "binance";
+  return shouldSkip ? true : false;
+};
+
+func.tags = ["SimpleManager"];
