273a0b4f263f7737e1bbae647cbc4840fd90d5e9
Gevarist <gevariste@protonmail.com>
2023-08-31 11:39:42 +0530

    deploy: all network

---
+++ deploy/SimpleManager.deploy.ts
@@ -0,0 +1,59 @@
+import { deployments, getNamedAccounts } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { getAddresses } from "../src/addresses";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  if (
+    hre.network.name === "mainnet" ||
+    hre.network.name === "polygon" ||
+    hre.network.name === "goerli" ||
+    hre.network.name === "optimism" ||
+    hre.network.name === "arbitrum" ||
+    hre.network.name === "binance" ||
+    hre.network.name === "base" ||
+    hre.network.name === "base_goerli" ||
+    hre.network.name === "sepolia"
+  ) {
+    console.log(
+      `Deploying SimpleManager to ${hre.network.name}. Hit ctrl + c to abort`
+    );
+    await new Promise((r) => setTimeout(r, 20000));
+  }
+
+  const { deploy } = deployments;
+  const { deployer, arrakisMultiSig, owner } = await getNamedAccounts();
+  const addresses = getAddresses(hre.network.name);
+
+  await deploy("SimpleManager", {
+    from: deployer,
+    proxy: {
+      proxyContract: "OpenZeppelinTransparentProxy",
+      owner: arrakisMultiSig,
+      execute: {
+        methodName: "initialize",
+        args: [owner],
+      },
+    },
+    args: [addresses.UniswapV3Factory],
+    log: true,
+  });
+};
+
+export default func;
+
+func.skip = async (hre: HardhatRuntimeEnvironment) => {
+  const shouldSkip =
+    hre.network.name === "mainnet" ||
+    hre.network.name === "polygon" ||
+    hre.network.name === "goerli" ||
+    hre.network.name === "optimism" ||
+    hre.network.name === "arbitrum" ||
+    hre.network.name === "binance" ||
+    hre.network.name === "base" ||
+    hre.network.name === "base_goerli" ||
+    hre.network.name === "sepolia";
+  return shouldSkip ? true : false;
+};
+
+func.tags = ["SimpleManager"];
--- src/addresses.ts
+++ src/addresses.ts
@@ -36,6 +36,21 @@ export const getAddresses = (network: string): Addresses => {
         UniswapV3Factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
         SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
       };
+    case "binance":
+      return {
+        UniswapV3Factory: "0xdB1d10011AD0Ff90774D0C6Bb92e5C5c8b4461F7",
+        SwapRouter: "0xB971eF87ede563556b2ED4b1C0b0019111Dd85d2",
+      };
+    case "base":
+      return {
+        UniswapV3Factory: "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",
+        SwapRouter: "0x2626664c2603336E57B271c5C0b26F421741e481",
+      };
+    case "sepolia":
+      return {
+        UniswapV3Factory: "0x0227628f3F023bb0B980b67D528571c95c6DaC1c",
+        SwapRouter: "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD",
+      };
     default:
       throw new Error(`No addresses for Network: ${network}`);
   }
