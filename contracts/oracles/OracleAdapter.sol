2379172fbbe6fcc7379a0f379765bbd0cbad99b8
Gevarist <gevariste@protonmail.com>
2024-02-22 13:47:11 +0100

    feat: oracle adapter

---
+++ contracts/oracles/OracleAdapter.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+import {AggregatorV3Interface} from "../interfaces/AggregatorV3Interface.sol";
+import {IERC4626Custom} from "../interfaces/IERC4626Custom.sol";
+import {FullMath} from "@arrakisfi/v3-lib-0.8/contracts/FullMath.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+// #region errors.
+
+error AddressZero();
+error GetAssetsPerShareFailed();
+
+// #endregion errors.
+
+contract OracleAdapter {
+    AggregatorV3Interface public immutable priceFeed;
+    address public immutable token;
+    bool public immutable ispriceFeedInversed;
+
+    constructor(
+        AggregatorV3Interface priceFeed_,
+        address token_,
+        bool ispriceFeedInversed_
+    ) {
+        if (address(priceFeed_) == address(0) || token_ == address(0))
+            revert AddressZero();
+        priceFeed = priceFeed_;
+        token = token_;
+        ispriceFeedInversed = ispriceFeedInversed_;
+    }
+
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        (roundId, answer, startedAt, updatedAt, answeredInRound) = priceFeed
+            .latestRoundData();
+        uint256 denominator = 1e18;
+        uint256 price = SafeCast.toUint256(answer);
+
+        try IERC4626Custom(token).assetsPerShare() returns (
+            uint256 assetsPerShare
+        ) {
+            if (ispriceFeedInversed) {
+                price = FullMath.mulDiv(price, denominator, assetsPerShare);
+            } else {
+                price = FullMath.mulDiv(price, assetsPerShare, denominator);
+            }
+        } catch {
+            revert GetAssetsPerShareFailed();
+        }
+
+        answer = SafeCast.toInt256(price);
+    }
+
+    function decimals() external view returns (uint8) {
+        return priceFeed.decimals();
+    }
+}
---
+++ test/foundry/OracleAdapter.t.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+import "../utils/TestWrapper.sol";
+import "forge-std/Vm.sol";
+import {
+    AggregatorV3Interface
+} from "../../contracts/interfaces/AggregatorV3Interface.sol";
+import {
+    IERC4626Custom
+} from "../../contracts/interfaces/IERC4626Custom.sol";
+import {OracleAdapter} from "../../contracts/oracles/OracleAdapter.sol";
+
+contract OracleAdapterTest is TestWrapper {
+    AggregatorV3Interface public constant apxETH_ETH =
+        AggregatorV3Interface(0x19219BC90F48DeE4d5cF202E09c438FAacFd8Bea);
+    address public constant apxETH = 0x9Ba021B0a9b958B5E75cE9f6dff97C7eE52cb3E6;
+    bool public constant ispriceFeedInversed = false;
+
+    OracleAdapter public oracleAdapter;
+
+    function setUp() public {
+        _reset(vm.envString("ETH_RPC_URL"), vm.envUint("ETH_BLOCK_NUMBER"));
+
+        oracleAdapter = new OracleAdapter(
+            apxETH_ETH,
+            apxETH,
+            ispriceFeedInversed
+        );
+    }
+
+    // #region test latestRoundData.
+
+    function testLatestRoundData() public {
+        (
+            ,
+            int256 answer,
+            ,
+            ,
+            
+        ) = oracleAdapter.latestRoundData();
+
+        console.logString("pxETH/ETH price : ");
+        console.logInt(answer);
+
+        (
+            ,
+            answer,
+            ,
+            ,
+            
+        ) = apxETH_ETH.latestRoundData();
+
+        console.logString("apxETH/ETH price : ");
+        console.logInt(answer);
+
+        console.logString("pxETH/apxETH price : ");
+        console.logUint(IERC4626Custom(apxETH).assetsPerShare());
+    }
+
+    // #endregion test latestRoundData.
+
+    // #region test decimals.
+
+    function testDecimals() public {
+        console.logUint(apxETH_ETH.decimals());
+        console.logUint(oracleAdapter.decimals());
+    }
+
+    // #endregion test decimals.
+}
